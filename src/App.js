1c2963c7f9664118b3cbe16e93e49b46cd101d59
Ahmed Elafifi <elafifi2015@gmail.com>
2022-10-28 00:43:04 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React v2 elafifi
         </a>
       </header>
     </div>
